67f1300eaf53af0ed7a4c1a679af7e758fa953cc
ImNotMenduina <mynameis_menduina@outlook.com>
2023-09-22 18:10:42 -0300

    add: threads idtfy

--- hello_threads.c
+++ hello_threads.c
@@ -4,15 +4,26 @@
 #include<unistd.h> 
 #include <sys/syscall.h>
 
-void * routine(){
-    printf("hello");
+void * routine(void* arg){
+    int* threadId = (int*)arg;
+    printf("hello from thread %d\n", *threadId);
+    sleep(3);
+    printf("Ending thread\n");
 }
 
 int main(int argc, char* argv[])
 {
-    pthread_t t1;
-    pthread_create (&t1, NULL, &routine, NULL);
-    pthread_join(t1, NULL);//wait for threads
+    pthread_t t1, t2;
+    int idt1 = 1;
+    int idt2 = 2;
+    if(pthread_create (&t1, NULL, &routine, (void*)&idt1) != 0)
+        return 1;
+    if(pthread_create (&t2, NULL, &routine, (void*)&idt2) != 0)
+        return 2;    
+    if(pthread_join(t1, NULL) != 0)//wait for thread t1
+        return 3;;
+    if(pthread_join(t2, NULL) != 0)//wait for thread t2
+        return 4;;
     return 0;
 }
 
